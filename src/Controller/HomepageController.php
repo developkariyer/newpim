a164b39a76b2d73b7ef12c9571acc7e928c5660c
enesbt <burdaenesvar@gmail.com>
2025-06-18 14:12:58 +0300

    custom pages

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Model\Document;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomePageController extends AbstractController
 {
