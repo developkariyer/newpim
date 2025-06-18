d60041bacc839e0d6b1960d3207777aaf6fd66ce
enesbt <burdaenesvar@gmail.com>
2025-06-18 13:50:52 +0300

    custom pages

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,7 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class HomePageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
