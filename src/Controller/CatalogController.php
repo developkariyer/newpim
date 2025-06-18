dcecd156cc5e2b0525f707af5af774b557dfa768
enesbt <burdaenesvar@gmail.com>
2025-06-18 16:11:43 +0300

    custom pages

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -6,13 +6,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Model\Document;
 use Symfony\Component\HttpFoundation\Request;
+use Pimcore\Model\DataObject\Product; 
+
 
 class CatalogController extends AbstractController
 {
     #[Route('/', name: 'catalog')]
     public function catalog(): Response
     {
-        return $this->render('catalog/catalog.html.twig'); 
+        $productsListing = new Product\Listing();
+        $productsListing->setOrderKey('name'); 
+        $productsListing->setOrder('ASC');    
+        $products = $productsListing->getObjects();
+        
+        return $this->render('catalog/catalog.html.twig', [
+            'products' => $products,
+        ]);
     }
 
 }
