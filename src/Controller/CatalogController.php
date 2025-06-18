80a69bc160fb82d8dc77f9e30a831337793c25f6
enesbt <burdaenesvar@gmail.com>
2025-06-18 16:16:42 +0300

    custom pages

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -1,27 +1,51 @@
 <?php
+
 namespace App\Controller;
 
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\DataObject\Category; 
+use Pimcore\Paginator\Adapter\DataObjectListing; 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Pimcore\Model\Document;
-use Symfony\Component\HttpFoundation\Request;
-use Pimcore\Model\DataObject\Product; 
-
+use Zend\Paginator\Paginator; 
 
 class CatalogController extends AbstractController
 {
+
     #[Route('/', name: 'catalog')]
-    public function catalog(): Response
+    public function catalog(Request $request): Response
     {
+        $page = (int) $request->query->get('page', 1);
+        $categoryId = (int) $request->query->get('category');
+        $searchTerm = $request->query->get('search');
+
         $productsListing = new Product\Listing();
-        $productsListing->setOrderKey('name'); 
-        $productsListing->setOrder('ASC');    
-        $products = $productsListing->getObjects();
+
+        if ($categoryId) {
+            $productsListing->setCondition('categories LIKE ?', ["%,$categoryId,%"]);
+        }
+
+        if ($searchTerm) {
+            $productsListing->setCondition(
+                '(name LIKE ? OR description LIKE ?)', 
+                ['%' . $searchTerm . '%', '%' . $searchTerm . '%']
+            );
+        }
+        $productsListing->setOrderKey('name');
+        $productsListing->setOrder('ASC');
+        $paginatorAdapter = new DataObjectListing($productsListing);
+        $paginator = new Paginator($paginatorAdapter);
+        $paginator->setItemCountPerPage(12);
+        $paginator->setCurrentPageNumber($page);
+        $categories = (new Category\Listing())->getObjects();
         
         return $this->render('catalog/catalog.html.twig', [
-            'products' => $products,
+            'paginator' => $paginator, 
+            'categories' => $categories, 
+            'currentCategoryId' => $categoryId, 
+            'currentSearchTerm' => $searchTerm, 
         ]);
     }
-
 }
