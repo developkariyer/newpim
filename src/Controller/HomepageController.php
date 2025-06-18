72a024bd22820954bcb27d36116a573b5ea84415
enesbt <burdaenesvar@gmail.com>
2025-06-18 14:10:21 +0300

    custom pages

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Pimcore\Model\Document;
 
 class HomePageController extends AbstractController
 {
@@ -11,5 +12,11 @@ class HomePageController extends AbstractController
     public function index(): Response
     {
         return $this->render('homepage/homepage.html.twig'); 
+    }
+
+    #[Route("/test", name: "test")]
+    public function test(Document $document, Request $request): Response
+    {
+        return $this->render($document->getTemplate());
     }
 }
