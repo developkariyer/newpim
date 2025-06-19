54d6051c139f9af7306899161fecffc6241846c1
enesbt <burdaenesvar@gmail.com>
2025-06-19 17:51:58 +0300

    Frontend Test

---
+++ src/Model/AdminStyle/ProductAdminStyle.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model\AdminStyle;
+
+use Pimcore\Model\DataObject;
+use Pimcore\Model\Element\AdminStyle;
+use Pimcore\Model\Element\ElementInterface;
+
+class ProductAdminStyle extends AdminStyle
+{
+    protected ElementInterface $element;
+
+    public function __construct(ElementInterface $element)
+    {
+        parent::__construct($element);
+
+        $this->element = $element;
+
+        if ($element instanceof \App\Model\DataObject\Product) {
+            DataObject\Service::useInheritedValues(true, function () use ($element) {
+                if ($element->getObjectType() == 'virtual') {
+                    $this->elementIcon = '/bundles/pimcoreadmin/img/twemoji/1f697.svg';
+                }
+            });
+        }
+    }
+}
---
+++ src/Model/DataObject/Product.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Model\DataObject;
+
+
+class Product extends \Pimcore\Model\DataObject\Product
+{
+   
+}
