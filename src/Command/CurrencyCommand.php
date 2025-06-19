217d96c28dc97f31340e29bb8a6faaeb30f2f310
enesbt <burdaenesvar@gmail.com>
2025-06-19 11:21:30 +0300

    LoggerFactory

--- src/Command/CurrencyCommand.php
+++ src/Command/CurrencyCommand.php
@@ -40,29 +40,29 @@ class CurrencyCommand extends AbstractCommand
                 $currencyName = $currency['CurrencyName'] ?? '';
                 $currencyCode = $currency['@attributes']['CurrencyCode'] ?? '';
                 if (empty($currencyName) || empty($currencyCode)) {
-                    $this->logger->error("❌ Missing currency name or code for date: {$date->format('Y-m-d')}");
+                    $this->logger->error("[" . __METHOD__ . "] ❌ Missing currency name or code for date: {$date->format('Y-m-d')}");
                     continue;
                 }
                 $rate = $currency['ForexBuying'] ?? $currency['ExchangeRate'] ?? null;
                 if ($rate === null) {
-                    $this->logger->error("❌ Missing rate for currency: $currencyName ($currencyCode) on {$date->format('Y-m-d')}");
+                    $this->logger->error("[" . __METHOD__ . "] ❌ Missing rate for currency: $currencyName ($currencyCode) on {$date->format('Y-m-d')}");
                     continue;
                 }
                 $currenyUnit = $currency['Unit'] ?? 0;
                 if ($currenyUnit <= 0) {
-                    $this->logger->error("❌ Invalid currency unit for: $currencyName ($currencyCode) on {$date->format('Y-m-d')}");
+                    $this->logger->error("[" . __METHOD__ . "] ❌ Invalid currency unit for: $currencyName ($currencyCode) on {$date->format('Y-m-d')}");
                     continue;
                 } 
                 try {
                     $rate = $rate / $currenyUnit;
-                    $this->logger->info("✅ Rate calculated for: $currencyName ($currencyCode) - Rate: $rate on {$date->format('Y-m-d')}");
+                    $this->logger->info("[" . __METHOD__ . "] ✅ Rate calculated for: $currencyName ($currencyCode) - Rate: $rate on {$date->format('Y-m-d')}");
                 } catch (\Throwable $e) {
-                    $this->logger->error("❌ Error calculating rate for: $currencyName ($currencyCode) - {$e->getMessage()}");
+                    $this->logger->error("[" . __METHOD__ . "] ❌ Error calculating rate for: $currencyName ($currencyCode) - {$e->getMessage()}");
                     continue;
                 }
                 $currencyObject = Currency::getByCurrencyCode($currencyCode, ['limit' => 1,'unpublished' => true]);
                 if (!$currencyObject) {
-                    $this->logger->info("❌ No existing currency object found for: $currencyName ($currencyCode), creating new one.");
+                    $this->logger->info("[" . __METHOD__ . "] ❌ No existing currency object found for: $currencyName ($currencyCode), creating new one.");
                     $currencyObject = new Currency();
                     $currencyObject->setParent(Folder::getByPath('/Ayarlar/Sabitler/Döviz-Kurları'));
                     $currencyObject->setKey(trim($currencyName));
@@ -72,7 +72,7 @@ class CurrencyCommand extends AbstractCommand
                 $currencyObject->setRate($rate);
                 $currencyObject->setDate($date);
                 $currencyObject->save();
-                $this->logger->info("✅ Currency object saved for: $currencyName ($currencyCode) with rate: $rate on {$date->format('Y-m-d')}");
+                $this->logger->info("[" . __METHOD__ . "] ✅ Currency object saved for: $currencyName ($currencyCode) with rate: $rate on {$date->format('Y-m-d')}");
             }
         }
         return Command::SUCCESS;
