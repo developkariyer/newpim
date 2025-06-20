513e5b68712b58b6d96fe4cbc17e14b5657ab52b
enesbt <burdaenesvar@gmail.com>
2025-06-20 17:14:39 +0300

    Product Model

--- src/Select/VariationSize.php
+++ src/Select/VariationSize.php
@@ -12,28 +12,26 @@ class VariationSize implements SelectOptionsProviderInterface
     public function getOptions(array $context, Data $fieldDefinition = null): array
     {
         $options = [];
-        error_log('Context keys: ' . implode(', ', array_keys($context)));
         if (isset($context['object']) && $context['object'] instanceof Product) {
             $currentObject = $context['object'];
-            error_log('Current object ID: ' . $currentObject->getId());
             $variantSizeTemplate = $currentObject->getVariantSizeTemplate();
-            error_log('Variant size template: ' . ($variantSizeTemplate ? $variantSizeTemplate->getId() : 'NULL'));
+            
             if ($variantSizeTemplate instanceof VariationSizeChart) {
-                $sizeOptionsData = $variantSizeTemplate->get('sizeOptions');
-                error_log('Direct get sizeOptions: ' . print_r($sizeOptionsData, true));
-                $sizeOptionsGetter = $variantSizeTemplate->getSizeOptions();
-                error_log('Getter sizeOptions: ' . print_r($sizeOptionsGetter, true));
-                $options[] = [
-                    'key' => 'Test Size',
-                    'value' => 'test'
-                ];
-            } else {
-                error_log('variantSizeTemplate is not VariationSizeChart instance');
+                $sizeOptionsData = $variantSizeTemplate->getSizeOptions();
+                
+                if (is_array($sizeOptionsData) && !empty($sizeOptionsData)) {
+                    foreach ($sizeOptionsData as $sizeOption) {
+                        if (is_array($sizeOption) && !empty($sizeOption[0])) {
+                            $sizeName = $sizeOption[0];
+                            $options[] = [
+                                'key' => $sizeName,
+                                'value' => $sizeName
+                            ];
+                        }
+                    }
+                }
             }
-        } else {
-            error_log('Object is not Product instance or not set');
         }
-        error_log('Final options count: ' . count($options));
         return $options;
     }
 
