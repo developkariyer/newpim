45153ff1737d2957426eb621b7f1c87bf7cf7e8e
enesbt <burdaenesvar@gmail.com>
2025-06-20 17:07:09 +0300

    Product Model

--- src/Select/VariationSize.php
+++ src/Select/VariationSize.php
@@ -4,45 +4,30 @@ namespace App\Select;
 
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\SelectOptionsProviderInterface;
-use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\Product;
+use Pimcore\Model\DataObject\VariationSizeChart;
 
 class VariationSize implements SelectOptionsProviderInterface
 {
     public function getOptions(array $context, Data $fieldDefinition = null): array
     {
         $options = [];
-        if (isset($context['object']) && $context['object'] instanceof DataObject\Concrete) {
+        if (isset($context['object']) && $context['object'] instanceof Product) {
             $currentObject = $context['object'];
             $variantSizeTemplate = $currentObject->getVariantSizeTemplate();
-            if ($variantSizeTemplate instanceof DataObject\Concrete) {
+            if ($variantSizeTemplate instanceof VariationSizeChart) {
                 $sizeOptionsTable = $variantSizeTemplate->getSizeOptions();
-                if ($sizeOptionsTable instanceof DataObject\Data\Table) {
-                    $sizeOptionsData = $sizeOptionsTable->getData();
-
-                    if (is_array($sizeOptionsData) && !empty($sizeOptionsData)) {
-                        foreach ($sizeOptionsData as $sizeOption) {
-                            $value = null;
-                            $label = null;
-
-                            if (is_array($sizeOption)) {
-                                $value = $sizeOption['key'] ?? $sizeOption['size'] ?? $sizeOption['value'] ?? null;
-                                $label = $sizeOption['label'] ?? $sizeOption['name'] ?? $value;
-                            } 
-                            elseif (is_object($sizeOption)) {
-                                $value = method_exists($sizeOption, 'getKey') ? $sizeOption->getKey() : 
-                                        (method_exists($sizeOption, 'getSize') ? $sizeOption->getSize() : 
-                                        (method_exists($sizeOption, 'getValue') ? $sizeOption->getValue() : null));
-                                $label = method_exists($sizeOption, 'getLabel') ? $sizeOption->getLabel() : 
-                                        (method_exists($sizeOption, 'getName') ? $sizeOption->getName() : $value);
-                            }
-
-                            if ($value !== null) { 
-                                $options[] = [
-                                    'key'   => $label, 
-                                    'value' => $value  
-                                ];
-                            }
-                        }
+                $sizeOptionsData = $sizeOptionsTable->getData();
+                foreach ($sizeOptionsData as $sizeOption) {
+                    $value = null;
+                    $label = null;
+                    $value = $sizeOption['key'] ?? $sizeOption['size'] ?? $sizeOption['value'] ?? null;
+                    $label = $sizeOption['label'] ?? $sizeOption['name'] ?? $value;
+                    if ($value !== null) { 
+                        $options[] = [
+                            'key'   => $label, 
+                            'value' => $value  
+                        ];
                     }
                 }
             }
