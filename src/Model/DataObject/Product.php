0abb9284f60f46f4993a4795f2c5f5f4c7702758
enesbt <burdaenesvar@gmail.com>
2025-06-20 14:17:42 +0300

    Product Model

---
+++ src/Model/DataObject/Product.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Model\DataObject;
+use Pimcore\Model\DataObject\Concrete;
+
+
+class Product extends Concrete
+{
+    const OBJECT_TYPE_ACTUAL = 'actual';
+    const OBJECT_TYPE_VIRTUAL = 'virtual';
+
+
+}
