ab00d1cece49edaa86c3bf2e9766a7f00a8a5d98
enesbt <burdaenesvar@gmail.com>
2025-06-20 16:56:07 +0300

    Product Model

--- src/Select/VariationSize.php
+++ src/Select/VariationSize.php
@@ -11,41 +11,43 @@ class VariationSize implements SelectOptionsProviderInterface
     public function getOptions(array $context, Data $fieldDefinition = null): array
     {
         $options = [];
-        
         if (isset($context['object']) && $context['object'] instanceof DataObject\Concrete) {
             $currentObject = $context['object'];
             $variantSizeTemplate = $currentObject->getVariantSizeTemplate();
             if ($variantSizeTemplate instanceof DataObject\Concrete) {
-                $sizeOptions = $variantSizeTemplate->getSizeOptions();
-                
-                if (is_array($sizeOptions) && !empty($sizeOptions)) {
-                    foreach ($sizeOptions as $sizeOption) {
-                        if (is_array($sizeOption)) {
-                            $value = $sizeOption['key'] ?? $sizeOption['size'] ?? $sizeOption['value'] ?? null;
-                            $label = $sizeOption['label'] ?? $sizeOption['name'] ?? $value;
-                        } elseif (is_object($sizeOption)) {
-                            $value = method_exists($sizeOption, 'getKey') ? $sizeOption->getKey() : 
-                                    (method_exists($sizeOption, 'getSize') ? $sizeOption->getSize() : 
-                                    (method_exists($sizeOption, 'getValue') ? $sizeOption->getValue() : null));
-                            $label = method_exists($sizeOption, 'getLabel') ? $sizeOption->getLabel() : 
-                                    (method_exists($sizeOption, 'getName') ? $sizeOption->getName() : $value);
-                        } else {
-                            // String veya başka tip ise
-                            $value = $sizeOption;
-                            $label = $sizeOption;
-                        }
-                        
-                        if ($value) {
-                            $options[] = [
-                                'key' => $label,
-                                'value' => $value
-                            ];
+                $sizeOptionsTable = $variantSizeTemplate->getSizeOptions();
+                if ($sizeOptionsTable instanceof DataObject\Data\Table) {
+                    $sizeOptionsData = $sizeOptionsTable->getData();
+
+                    if (is_array($sizeOptionsData) && !empty($sizeOptionsData)) {
+                        foreach ($sizeOptionsData as $sizeOption) {
+                            $value = null;
+                            $label = null;
+
+                            if (is_array($sizeOption)) {
+                                $value = $sizeOption['key'] ?? $sizeOption['size'] ?? $sizeOption['value'] ?? null;
+                                $label = $sizeOption['label'] ?? $sizeOption['name'] ?? $value;
+                            } 
+                            elseif (is_object($sizeOption)) {
+                                $value = method_exists($sizeOption, 'getKey') ? $sizeOption->getKey() : 
+                                        (method_exists($sizeOption, 'getSize') ? $sizeOption->getSize() : 
+                                        (method_exists($sizeOption, 'getValue') ? $sizeOption->getValue() : null));
+                                $label = method_exists($sizeOption, 'getLabel') ? $sizeOption->getLabel() : 
+                                        (method_exists($sizeOption, 'getName') ? $sizeOption->getName() : $value);
+                            }
+
+                            if ($value !== null) { 
+                                $options[] = [
+                                    'key'   => $label, 
+                                    'value' => $value  
+                                ];
+                            }
                         }
                     }
                 }
             }
         }
-        
+
         return $options;
     }
 
