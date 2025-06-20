66d9d7e781d4fb32fe9108d92730cdea414ffcfa
enesbt <burdaenesvar@gmail.com>
2025-06-20 09:00:13 +0300

    Product Model

--- src/EventListener/PimcoreAdminListener.php
+++ src/EventListener/PimcoreAdminListener.php
@@ -5,14 +5,25 @@ namespace App\EventListener;
 
 use Pimcore\Bundle\AdminBundle\Event\ElementAdminStyleEvent;
 use Pimcore\Event\BundleManager\PathsEvent;
+use Psr\Log\LoggerInterface;
 
 class PimcoreAdminListener
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
 
     public function onResolveElementAdminStyle(ElementAdminStyleEvent $event): void
     {
+        $this->logger->info('Listener tetiklendi!');
         $element = $event->getElement();
+         $this->logger->info('Element tipi: ' . get_class($element));
         if ($element instanceof \App\Model\DataObject\Product) {
+            $this->logger->info('Product bulundu!');
+            $this->logger->info('ObjectType: ' . $element->getObjectType());
             $event->setAdminStyle(new \App\Model\AdminStyle\ProductAdminStyle($element));
         }
     }
