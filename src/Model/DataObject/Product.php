454fa5199da423f30f84cc765e4b516b3a9609eb
enesbt <burdaenesvar@gmail.com>
2025-06-20 08:23:30 +0300

    Model

--- src/Model/DataObject/Product.php
+++ src/Model/DataObject/Product.php
@@ -6,5 +6,13 @@ namespace App\Model\DataObject;
 
 class Product extends \Pimcore\Model\DataObject\Product
 {
-   
+    const OBJECT_TYPE_ACTUAL = 'actual';
+    const OBJECT_TYPE_VIRTUAL = 'virtual';
+
+
+    public function getOSIndexType(): string
+    {
+        return $this->getObjectType() === self::OBJECT_TYPE_ACTUAL ? self::OBJECT_TYPE_VARIANT : self::OBJECT_TYPE_OBJECT;
+    }
+
 }
