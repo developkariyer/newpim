d3bf1088b39559abefbe06ce5b4d04240c927d53
enesbt <burdaenesvar@gmail.com>
2025-06-24 11:35:57 +0300

    variant create

--- src/EventListener/ProductSaveListener.php
+++ src/EventListener/ProductSaveListener.php
@@ -27,13 +27,21 @@ class ProductSaveListener
             return;
         }
 
+        \error_log('matrix-debug', "Listener running for product: " . $object->getKey());
+
         $newMatrix = $this->variationMatrixService->generateMatrix($object);
-        \error_log("Matrix row count: " . count($newMatrix));
+        
+        \error_log('matrix-debug', "Generated matrix count: " . count($newMatrix));
+
         if (!empty($newMatrix)) {
+            \error_log('matrix-debug', "Matrix is NOT empty. Preparing to save.");
             $structuredTable = new StructuredTable();
             $structuredTable->setData($newMatrix);
             $object->setVariationMatrix($structuredTable);
             $object->save(['disableEvents' => true]);
-        } 
+            \error_log('matrix-debug', "Save command executed.");
+        } else {
+            \error_log('matrix-debug', "Matrix IS EMPTY. Nothing to save.");
+        }
     }
 }
