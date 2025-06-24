477749f806b55cdfc02b7477bd4c3a2efe61c156
enesbt <burdaenesvar@gmail.com>
2025-06-24 13:37:55 +0300

    variant

--- src/EventListener/ProductSaveListener.php
+++ src/EventListener/ProductSaveListener.php
@@ -37,7 +37,7 @@ class ProductSaveListener
                 return;
             }
 
-            $variationMatrix->setData($matrixData);
+            $variationMatrix->setData([$matrixData]);
 
         }
     }
