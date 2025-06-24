283a336f296099657d3a71f756fb2007100d02c7
enesbt <burdaenesvar@gmail.com>
2025-06-24 17:30:49 +0300

    frontend design

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Model\Document;
 use Symfony\Component\HttpFoundation\Request;
 use Pimcore\Model\DataObject\Product; 
+use Pimcore\Model\DataObject\Category\Listing as CategoryListing;
 
 
 class ProductController extends AbstractController
@@ -14,7 +15,23 @@ class ProductController extends AbstractController
     #[Route('/product', name: 'product')]
     public function index(): Response
     {
-        return $this->render('product/product.html.twig');
+        $categories = $this->getCategories();
+        return $this->render('product/product.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+
+    private function getCategories()
+    {
+        $categories = new CategoryListing();
+        $categories->load();
+        $categoryList = [];
+        foreach ($categories as $category) {
+            $categoryList[] = [
+                'id' => $category->getId(),
+                'name' => $category->getCategory(),
+            ];
+        }
     }
 
 }
