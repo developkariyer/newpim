8199e44ab4b0ca2d79409085163fa2af96cd6d80
enesbt <burdaenesvar@gmail.com>
2025-06-24 10:36:10 +0300

    variant create

--- src/Service/VariationMatrixService.php
+++ src/Service/VariationMatrixService.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use Pimcore\Model\DataObject\Product;
 use Pimcore\Model\DataObject\VariationSizeChart;
 use Pimcore\Model\DataObject\CustomChart;
+use Pimcore\Model\DataObject\Data\StructuredTable;
 
 class VariationMatrixService
 {
