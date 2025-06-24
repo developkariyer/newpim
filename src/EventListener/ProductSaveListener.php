18b3797b227d73afb3716f60e03f1cbd424e0ba9
enesbt <burdaenesvar@gmail.com>
2025-06-24 11:42:17 +0300

    variant create

--- src/EventListener/ProductSaveListener.php
+++ src/EventListener/ProductSaveListener.php
@@ -35,11 +35,40 @@ class ProductSaveListener
 
         if (!empty($newMatrix)) {
             \error_log("Matrix is NOT empty. Preparing to save.");
+            
+            // Matrix içeriğini logla
+            \error_log("Matrix content: " . print_r($newMatrix, true));
+            
             $structuredTable = new StructuredTable();
             $structuredTable->setData($newMatrix);
+            
+            // setData'dan sonra StructuredTable'ın içeriğini kontrol et
+            $retrievedData = $structuredTable->getData();
+            \error_log("StructuredTable after setData: " . print_r($retrievedData, true));
+            
             $object->setVariationMatrix($structuredTable);
+            
+            // setVariationMatrix'den sonra Product'tan geri oku
+            $checkMatrix = $object->getVariationMatrix();
+            if ($checkMatrix) {
+                $checkData = $checkMatrix->getData();
+                \error_log("Product matrix after set: " . print_r($checkData, true));
+            } else {
+                \error_log("Product matrix after set: NULL");
+            }
+            
             $object->save(['disableEvents' => true]);
             \error_log("Save command executed.");
+            
+            // Save'den sonra tekrar kontrol et
+            $finalMatrix = $object->getVariationMatrix();
+            if ($finalMatrix) {
+                $finalData = $finalMatrix->getData();
+                \error_log("Product matrix after save: " . print_r($finalData, true));
+            } else {
+                \error_log("Product matrix after save: NULL");
+            }
+            
         } else {
             \error_log("Matrix IS EMPTY. Nothing to save.");
         }
