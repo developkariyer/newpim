6974ccc9eba9f8ab6224015d6b7c1be6c22abca2
enesbt <burdaenesvar@gmail.com>
2025-06-26 14:03:59 +0300

    catalog page

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Pimcore\Model\Document;
