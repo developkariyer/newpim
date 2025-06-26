13cb350bf3bfab3aee5f3f676c141deff833d307
enesbt <burdaenesvar@gmail.com>
2025-06-26 13:35:51 +0300

    catalog page

--- src/Controller/CatalogController.php
+++ src/Controller/CatalogController.php
@@ -14,13 +14,38 @@ class CatalogController extends AbstractController
     #[Route('/', name: 'catalog')]
     public function catalog(): Response
     {
-        $productsListing = new Product\Listing();
-        $productsListing->setOrderKey('name'); 
-        $productsListing->setOrder('ASC');    
-        $products = $productsListing->getObjects();
+        $products = $this->getProducts(20, 0);
         return $this->render('catalog/catalog.html.twig', [
-            'products' => $products,
+            'products' => $products['products'],
+            'total' => $products['total']
         ]);
+    }
+
+    private function getProduct($limit, $offset, $condition = "published = 1"): array
+    {
+        $productsListing = new Product\Listing();
+        $productsListing->setLimit($limit);
+        $productsListing->setOffset($offset);
+        $productsListing->setOrderKey('name');
+        $productsListing->setOrder('asc');
+        $productsListing->setCondition($condition);
+        $totalCount = $productsListing->getTotalCount();
+        $products = $productsListing->load();
+        $formattedProducts = [];
+        foreach ($products as $product) {
+            $formattedProducts[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'image' => $product->getImage(),
+                'productIdentifier' => $product->getProductIdentifier(),
+                'category' => $product->getCategory()
+            ];
+        }
+
+        return [
+            'products' => $formattedProducts,
+            'total' => $totalCount,
+        ];
     }
 
 }
