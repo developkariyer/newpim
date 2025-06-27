959cc6ac9dcb39a5526e1d9eae07fe27e2c36162
enesbt <burdaenesvar@gmail.com>
2025-06-27 11:03:35 +0300

    transfer

--- src/Model/AdminStyle/ProductAdminStyle.php
+++ src/Model/AdminStyle/ProductAdminStyle.php
@@ -2,27 +2,65 @@
 
 namespace App\Model\AdminStyle;
 
-use Pimcore\Model\DataObject;
 use Pimcore\Model\Element\AdminStyle;
 use Pimcore\Model\Element\ElementInterface;
+use Pimcore\Model\DataObject\Product;
 
 class ProductAdminStyle extends AdminStyle
 {
+    /** @var ElementInterface */
     protected ElementInterface $element;
 
-    public function __construct(ElementInterface $element)
+    public function __construct($element)
     {
         parent::__construct($element);
 
         $this->element = $element;
 
-        if ($element instanceof \Pimcore\Model\DataObject\Product) {
-            if ($element->getObjectType() == 'actual') {
-                $this->elementIcon = '/bundles/pimcoreadmin/img/flat-color-icons/variant.svg';
-            }
-            if ($element->getObjectType() == 'virtual') {
-                $this->elementIcon = '/bundles/pimcoreadmin/img/flat-color-icons/object.svg';
+        if ($element instanceof Product) {
+            $this->elementIcon = '/custom/product.svg';
+            if ($element->level() === 1) {
+                $this->elementIcon = '/custom/object.svg';
+                if (count($element->getListingItems()) + count($element->getBundleProducts())) {
+                    $this->elementIcon = '/custom/deployment.svg';
+                }
             }
         }
+    }
+
+    public function getElementQtipConfig(): ?array
+    {
+        if ($this->element instanceof Product) {
+            $config = parent::getElementQtipConfig();
+            $config['title'] = "{$this->element->getId()}: {$this->element->getName()}";
+            $shopifyVariations = $total = count($this->element->getListingItems());
+            foreach ($this->element->getChildren() as $child) {
+                if ($child instanceof Product) {
+                    $shopifyVariations += count($child->getListingItems());
+                }
+            }
+            $config['text'] = "$total/$shopifyVariations listing bağlı<br>";
+            $image = $this->element->getInheritedField('image');
+            if ($image) {
+                $config["text"] .= "<img src='{$image->getThumbnail()->getPath()}' style='max-width: 100%; height: 100px; background-color: #f0f0f0;' alt='alt'>";
+            }
+            $album = $this->element->getInheritedField('album');
+            foreach ($album as $asset) {
+                if (!$asset) {
+                    continue;
+                }
+                $image = $asset->getImage();
+                if ($image) {
+                    $config['text'] .= "<img src='{$image->getThumbnail()->getPath()}' style='max-width: 100%; height: 100px; background-color: #f0f0f0;' alt='alt'>";
+                    break;
+                }
+            }
+            $imageUrl = $this->element->getInheritedField('imageUrl');
+            if ($imageUrl) {
+                $config['text'] .= "<img src='{$imageUrl->getUrl()}' style='max-width: 100%; height: 100px; background-color: #f0f0f0;' alt=\"alt\">";
+            }
+            return $config;
+        }
+        return parent::getElementQtipConfig();
     }
 }
