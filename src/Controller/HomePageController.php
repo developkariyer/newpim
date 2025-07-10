e61a09d94ed7690698ba6d1c643de8ebbf8d0992
enesbt <burdaenesvar@gmail.com>
2025-07-10 09:26:40 +0300

    catalog

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
     public function homepage(): Response
     {
-        $user = $this->getUser();
+        $lastUsername = $authenticationUtils->getLastUsername();
         return $this->render('homepage/homepage.html.twig', [
-            'user' => $user,
+            'user' => $lastUsername,
         ]);
     }
 }
