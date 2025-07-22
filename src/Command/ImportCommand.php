b47eef7c4243a8732de858f90149d2bec260ba8c
enesbt <burdaenesvar@gmail.com>
2025-07-22 13:27:40 +0300

    import data

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -20,24 +20,28 @@ class ImportCommand extends AbstractCommand
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $filePath = PIMCORE_PROJECT_ROOT . '/tmp/exportProduct.json';
-
         if (!file_exists($filePath)) {
             $output->writeln('<error>File not found: ' . $filePath . '</error>');
             return Command::FAILURE;
         }
-
         $jsonContent = file_get_contents($filePath);
         $data = json_decode($jsonContent, true);
-
         if (json_last_error() !== JSON_ERROR_NONE) {
             $output->writeln('<error>JSON decode error: ' . json_last_error_msg() . '</error>');
             return Command::FAILURE;
         }
-
-        $prettyJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        $output->writeln('<info>Imported Data (JSON):</info>');
-        $output->writeln($prettyJson);
-
+        if (!is_array($data)) {
+            $output->writeln('<error>Decoded JSON is not an array.</error>');
+            return Command::FAILURE;
+        }
+        $output->writeln('<info>Products:</info>');
+        foreach ($data as $index => $product) {
+            $output->writeln("Product #$index");
+            $output->writeln("ID: " . ($product['id'] ?? 'N/A'));
+            $output->writeln("Name: " . ($product['name'] ?? 'N/A'));
+            $output->writeln("Price: " . ($product['price'] ?? 'N/A'));
+            $output->writeln('---------------------------');
+        }
         return Command::SUCCESS;
     }
 
