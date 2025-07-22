2873519c95d9d733c020ceaeaf4f003344c502f7
enesbt <burdaenesvar@gmail.com>
2025-07-22 13:19:25 +0300

    import data

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -8,11 +8,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Pimcore\Model\DataObject\Currency;
 use Symfony\Component\Console\Input\InputOption;
-use Pimcore\Model\DataObject\Folder;
-use App\Logger\LoggerFactory;
-use App\Service\DatabaseService;
 
 #[AsCommand(
     name: 'app:import',
@@ -23,9 +19,24 @@ class ImportCommand extends AbstractCommand
     
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $content = file_get_contents(PIMCORE_PROJECT_ROOT . '/tmp/exportProduct.json');
-        $content = json_decode($content, true);
-        print_r($content);
+        $filePath = PIMCORE_PROJECT_ROOT . '/tmp/exportProduct.json';
+
+        if (!file_exists($filePath)) {
+            $output->writeln('<error>File not found: ' . $filePath . '</error>');
+            return Command::FAILURE;
+        }
+
+        $jsonContent = file_get_contents($filePath);
+        $data = json_decode($jsonContent, true);
+
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            $output->writeln('<error>JSON decode error: ' . json_last_error_msg() . '</error>');
+            return Command::FAILURE;
+        }
+
+        $prettyJson = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+        $output->writeln('<info>Imported Data (JSON):</info>');
+        $output->writeln($prettyJson);
 
         return Command::SUCCESS;
     }
