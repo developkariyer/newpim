d6bf261d6bc6dcdca9bc39b2a5154cd1af46818c
enesbt <burdaenesvar@gmail.com>
2025-07-30 17:57:02 +0300

    refactor

--- public/assets/js/product.js
+++ public/assets/js/product.js
@@ -1366,34 +1366,19 @@ class VariationService {
             if (filteredArrays.length === 0) return [];
             return filteredArrays.reduce((a, b) => a.flatMap(d => b.map(e => [...(Array.isArray(d) ? d : [d]), e])));
         };
-
-        const combinations = [];
-
-        if (colors.length > 0 && sizes.length > 0 && customs.length === 0) {
-            combinations.push(...cartesianProduct([colors, sizes]).map(item => [item[0], item[1], null]));
+        if (colors.length > 0 && sizes.length === 0 && customs.length === 0) {
+            return colors.map(color => [color, null, null]);
         }
         if (colors.length > 0 && sizes.length === 0 && customs.length > 0) {
-            combinations.push(...cartesianProduct([colors, customs]).map(item => [item[0], null, item[1]]));
+            return cartesianProduct([colors, customs]).map(item => [item[0], null, item[1]]);
         }
-        if (colors.length === 0 && sizes.length > 0 && customs.length > 0) {
-            combinations.push(...cartesianProduct([sizes, customs]).map(item => [null, item[0], item[1]]));
+        if (colors.length > 0 && sizes.length > 0 && customs.length === 0) {
+            return cartesianProduct([colors, sizes]).map(item => [item[0], item[1], null]);
         }
-
         if (colors.length > 0 && sizes.length > 0 && customs.length > 0) {
-            combinations.push(...cartesianProduct([colors, sizes, customs]));
+            return cartesianProduct([colors, sizes, customs]);
         }
-
-        if (colors.length > 0 && sizes.length === 0 && customs.length === 0) {
-            combinations.push(...colors.map(color => [color, null, null]));
-        }
-        if (sizes.length > 0 && colors.length === 0 && customs.length === 0) {
-            combinations.push(...sizes.map(size => [null, size, null]));
-        }
-        if (customs.length > 0 && colors.length === 0 && sizes.length === 0) {
-            combinations.push(...customs.map(custom => [null, null, custom]));
-        }
-
-        return combinations;
+        return [];
     }
 
     generateMatrixHTML(combos, sizesData, customs) {
